46bc7cd16b2c5762138b80cd30660399d6b52362
Siva-viswanadhan <sivaviswanadhan7@gmail.com>
2026-01-03 15:03:15 +0530

    update Python kernel display name in Jupyter notebook

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_classic.chains.combine_documents import create_stuff_documents_chain
-from langchain_classic.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 # -------------------------------
